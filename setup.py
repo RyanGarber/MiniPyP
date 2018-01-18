803749b9ca199f815f3a0f048ecb1b7e270d384f
Ryan <ryanmichaelgarber@gmail.com>
2018-01-18 04:50:25 -0500

    Clean up documentation

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
     ],
     keywords='http https web cgi fast-cgi fpm ssl tls socket proxy reverse',
     packages=find_packages(exclude=['docs', 'tests']),
+    download_url='https://github.com/RyanGarber/minipyp/archive/0.1.0a1.tar.gz',
     install_requires=['requests', 'h2'],
     extras_require={'tests': ['unittest']},
     python_requires='>=3'
